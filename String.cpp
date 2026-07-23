dab48aad8e9630404a4a05dbd1e357715d685fc0
Shaarav Raghu <shaaravraghu@gmail.com>
2026-07-23 22:34:23 +0530

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -4,3 +4,17 @@ string s(5, 'a'); // aaaaa
 string s="";
 
 getline(cin, s); // to get full string
+
+s[5];
+s.front();
+s.back();
+s.length();
+s.size();
+s[i] = x;
+
+reverse(s.begin(), s.end());
+sort(s.begin(), s.end());
+count(s.begin(), s.end(), 'a');
+s.find("xyz");
+s.substr(start, length);
+
