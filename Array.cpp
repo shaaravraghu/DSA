aea23bf4097d478a3852972266fc032e30488289
Shaarav Raghu <shaaravraghu@gmail.com>
2026-07-29 10:33:01 +0530

    Implement array algorithms in Array.cpp
    Added various array traversal and manipulation algorithms including cumulative sums, frequency count, and Kadane's algorithm.

--- Array.cpp
+++ Array.cpp
@@ -38,29 +38,35 @@ binary_search(arr.begin(), arr.end(), x);
 
 // LINEAR TRAVERSAL
 for (int x:arr){}
+
 // MAX/ MIN
 for (int x:arr){
   val = (x > val) ? x: val;
   \\ OR
   val = max(val, x);
 }
+
 // CUMULATIVE
 for (int i=0; i<arr.size(); i++){
   cum[i]=cum[i-1]+arr[i];
 }
+
 // CUMULATIVE MAX/ MIN
 for (int i=0; i<arr.size(); i++){
   cum[i]= max(cum[i-1],arr[i]);
 }
+
 // FREQUENCY
 for(int x:arr){
   freq[x]++;
 }
+
 // KADANE'S ALGORITHM (LARGEST CONTINUOUS SUB-ARRAY)
 for(int i=0; i<arr.size(); i++){
   current_streak = max(current_streak, current_streak + arr[i]);
   best = max(best, current_streak);
 }
+
 // DIFFERENCE ARRAY
 for (int i=1; i<arr.size(); i++){
   diff[i-1]= arr[i] - arr[i-1];
