7cc74ad5803afb07f89fc67dd25f5738a04ac5c7
Shaarav Raghu <shaaravraghu@gmail.com>
2026-07-31 17:25:30 +0530

    Create Miscellaneous.cpp

---
+++ Miscellaneous.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() { // used to randomly generate the smallest/ largest possible value for a variable
+    cout << INT_MIN << endl; // -2147483648
+    cout << INT_MAX << endl; // 2147483647
+}
