add62cbdd91e69e502fc68c73dec244f65ed1cb7
Shaarav Raghu <shaaravraghu@gmail.com>
2026-07-31 21:03:48 +0530

    Update Two_Pointers.cpp

--- Two_Pointers.cpp
+++ Two_Pointers.cpp
@@ -197,3 +197,16 @@ return pq.size();
 // Remove meetings that ended
 // Heap size = rooms occupied
 // Maximum heap size = answer
+
+// Interval Intersection: Converge two list of intervals
+int i = 0, j = 0;
+while (i < A.size() && j < B.size()) {
+    int l = max(A[i][0], B[j][0]);
+    int r = min(A[i][1], B[j][1]);
+    if (l <= r)
+        ans.push_back({l, r});
+    if (A[i][1] < B[j][1])
+        i++;
+    else
+        j++;
+}
