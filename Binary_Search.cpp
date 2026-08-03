575395db1992faa55ff4c387ed0f29f66e4a7316
Shaarav Raghu <shaaravraghu@gmail.com>
2026-08-03 19:52:58 +0530

    Improve binary search lower and upper bound logic
    Refactor binary search implementation for clarity and correctness.

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -12,11 +12,21 @@ while (l <= r) {
 } return -1;
 
 // Lower Bound: First Element < Target
-int l = 0, r = a.size(); // this defines the solution to belong in [l, r) and when r-l = 1 we have only possible solution as l
-while (l < r) {
+int l = 0, r = a.size(); // this defines the solution to belong in [l, r) 
+while (l < r) { // when r-l = 0, the search is over; it doesn't define the solution [[l, r) -> [l, l) -> NULL]
     int mid = l + (r - l) / 2;
     if (a[mid] < target)
         l = mid + 1; // mid+1 because a[mid] < target
     else // a[mid] >= target
         r = mid; // mid because a[mid] = target is a possiblity
-} return l; 
+} return l; // or return 'r' because l == r
+
+// Upper Bound: First Element > Target
+int l = 0, r = a.size();
+while (l < r) {
+    int mid = l + (r - l) / 2;
+    if (a[mid] <= target)
+        l = mid + 1;
+    else
+        r = mid;
+} return l;   // or return r;
