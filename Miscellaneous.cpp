79afd5a226bf08d35633d9848806e77fe46fac6e
Shaarav Raghu <shaaravraghu@gmail.com>
2026-08-03 15:11:27 +0530

    Update Miscellaneous.cpp

--- Miscellaneous.cpp
+++ Miscellaneous.cpp
@@ -6,3 +6,12 @@ int main() { // used to randomly generate the smallest/ largest possible value f
     cout << INT_MIN << endl; // -2147483648
     cout << INT_MAX << endl; // 2147483647
 }
+
+// k-sum target: recursive algorithm
+vector<vector<int>> kSum(nums, start, k, target)
+{
+    if (k == 2)
+        return TwoPointer();
+    for (int i = start; i < n; i++)
+        recurse(i + 1, k - 1, target - nums[i]);
+}
