59436fb2b5b6efc31332dcfa30780e7f98e120f3
Shaarav Raghu <shaaravraghu@gmail.com>
2026-08-08 18:40:03 +0530

    Implement Bubble Sort algorithm
    Added Bubble Sort implementation to Sort.cpp.

--- Sort.cpp
+++ Sort.cpp
@@ -32,3 +32,33 @@ PARTITION(A, low, high):
 
     swap(A[i + 1], A[high])
     return i + 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Bubble Sort
+BUBBLE_SORT(A, n):
+
+    for i = 0 to n - 2:
+        swapped = false
+
+        for j = 0 to n - i - 2:
+
+            if A[j] > A[j + 1]:
+                swap(A[j], A[j + 1])
+                swapped = true
+
+        if swapped == false:
+            break
